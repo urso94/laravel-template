f90613dfd97907ed4a6cfe55350dcae2fb75b2b7
Maurizio Urso <m.urso@sermetra.it>
2023-12-22 15:16:28 +0100

    feat: quando un tenant non è configurato restituiamo un 503

--- code/app/Http/Middleware/Tenant.php
+++ code/app/Http/Middleware/Tenant.php
@@ -11,12 +11,16 @@ class Tenant
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param Closure(Request): (Response) $next
      */
     public function handle(Request $request, Closure $next): Response
     {
         if (!$tenant = $request->header('x-tenant')) {
             return response()->json(['message' => 'Tenant not provided'], 503);
+        }
+
+        if (!\App\Models\Tenant::where('code', '=', $tenant)->first()) {
+            return response()->json(['message' => 'Tenant not configured'], 503);
         }
 
         return $next($request);
